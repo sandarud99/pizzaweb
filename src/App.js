0cdffb8e3f462232eca8bed04f078cff32ea19ad
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-06-24 14:56:46 +0530

    Added styles for pages and modified pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Switch>
         <Footer />
       </Router>
